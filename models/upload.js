f148bff043c1a8853bc6a334f36c60b888694bae
G Menakshi <kshimeena437@gmail.com>
2025-06-30 23:19:38 +0530

    Update upload.js

--- models/upload.js
+++ models/upload.js
@@ -1,31 +1,13 @@
+// models/upload.js
 const mongoose = require('mongoose');
 
 const uploadSchema = new mongoose.Schema({
- imageUrl: {
-    type: String,
-    required: false,
-  },
-  videoUrl: {
-    type: String,
-    required: false, // New: for video posts
-  },
-  audioUrl: {
-    type: String,
-    required: false, // New: for voice posts
-  },
-  textContent: {
-    type: String,
-    required: false, // New: for text posts
-  },
-  caption: {
-    type: String,
-    required: false,
-  },
-  music: {
-    type: String,
-    required: false,
-  },
-
+  imageUrl: String,
+  videoUrl: String,
+  audioUrl: String,
+  textContent: String,
+  caption: String,
+  music: String,
 });
 
 module.exports = mongoose.model('Upload', uploadSchema);
