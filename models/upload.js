5f0165a8cbeba6f2b48c1a72ec933c15b29d6e6d
G Menakshi <kshimeena437@gmail.com>
2025-06-30 22:44:22 +0530

    Create upload.js

---
+++ models/upload.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+ imageUrl: {
+    type: String,
+    required: false,
+  },
+  videoUrl: {
+    type: String,
+    required: false, // New: for video posts
+  },
+  audioUrl: {
+    type: String,
+    required: false, // New: for voice posts
+  },
+  textContent: {
+    type: String,
+    required: false, // New: for text posts
+  },
+  caption: {
+    type: String,
+    required: false,
+  },
+  music: {
+    type: String,
+    required: false,
+  },
+
+});
+
+module.exports = mongoose.model('Upload', postSchema);
