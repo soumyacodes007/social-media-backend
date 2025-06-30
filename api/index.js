99f77a2b957fa4c12f878cbc0b1a730a3a932c86
soumyacodes007 <rickbhowmik4@gmail.com>
2025-07-01 00:16:31 +0530

    fixed baptics error

--- api/index.js
+++ api/index.js
@@ -301,5 +301,6 @@ app.delete("/api/uploads/:id", async (req, res) => {
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-console.log(✅ Server is listening on port ${PORT});
+// eta baptics er vetor hobe
+console.log(`✅ Server is listening on port ${PORT}`);
 });
