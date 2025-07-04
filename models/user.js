04c17f6c4e568dc68d4783214166479d2c244fee
G Menakshi <kshimeena437@gmail.com>
2025-07-04 14:48:13 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  phone: { type: String, unique: true, required: true },
+  name: String,
+  profileImage: String,
+  isOnline: { type: Boolean, default: false },
+  lastSeen: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
