8a0867175684e1f70f7e5f25a32f89c3dfbc6993
soumyacodes007 <rickbhowmik4@gmail.com>
2025-07-06 01:03:16 +0530

    added follower count

--- models/user.js
+++ models/user.js
@@ -1,11 +1,30 @@
+// models/user.js
+
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  phone: { type: String, unique: true, required: true },
-  name: String,
-  profileImage: String,
-  isOnline: { type: Boolean, default: false },
-  lastSeen: { type: Date, default: Date.now },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    phone: { type: String, unique: true, required: true },
+    name: String,
+    profileImage: String, // You have 'profileImage', we will use this.
+    isOnline: { type: Boolean, default: false },
+    lastSeen: { type: Date, default: Date.now },
+
+    
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // This links to another document in the same User collection
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // This also links to another User
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
