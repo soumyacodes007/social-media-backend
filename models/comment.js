fb64fbe8c0056189b85f428225b8cfe359ac578a
soumyacodes007 <rickbhowmik4@gmail.com>
2025-07-05 23:25:25 +0530

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  text: String, // ✅ match this with backend field
-  createdAt: { type: Date, default: Date.now },
+  
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post", 
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
